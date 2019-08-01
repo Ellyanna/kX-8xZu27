98bbb52047e679888471dc0dd0fd889be24c17b3
Anokino <45012641+Anokino@users.noreply.github.com>
2019-08-01 22:59:19 +0200

    finish

--- Shi-Fu-Mi.py
+++ Shi-Fu-Mi.py
@@ -1,11 +1,14 @@
 from tkinter import*
 from random import*
 import tkinter as tk
+import sys
+import os
 propos2= "n"
 
 class Root (Tk):
 	def __init__(self) :
 		super().__init__()
+
 		policeMenu = ('Helvetic', 10, 'bold')
 		policeOpts = ('Arial', 7, 'italic')
 
@@ -13,50 +16,53 @@ class Root (Tk):
 
 		menu1 = tk.Menu(menuBarre, tearoff=0)
 		menu1.add_command(label="À propos", font=policeMenu, command = self.propos)
+		menu1.add_command(label = "Menu principal", font=policeMenu, command = self.restart_program)
 		menu1.add_separator()
 		menu1.add_command(
 			label="Quitter",font=policeMenu, command = self.all)
 
-		menuBarre.add_cascade(label="Option", font=policeOpts, menu=menu1)
+		menuBarre.add_cascade(label="Options", font=policeOpts, menu=menu1)
+
 
 		self.config(menu=menuBarre)
 
 		self.title("Shi-Fu-Mi")
-		#self.root.iconbitmap("icons8-manette-256.png")
 		self.iconbitmap(bitmap="shifumi.ico")
-		self.geometry("354x460")
+		self.geometry("352x235")
 		self.points = 0
 		self.points2j1 = 0
 		self.points2j2 = 0
 		self.messageD=Label(self, text = "Shi-fu-mi : 1 ou 2 joueurs ?", font = "Ar")
 		self.messageD.grid(row=1, columnspan=3)
-		self.bouton1j = Button(self, text = '1 joueur', command = self.interface1j)
-		self.bouton1j.grid(row= 2, column = 1)
-		self.bouton2j = Button(self, text = '2 joueurs', command = self.interface2j)
-		self.bouton2j.grid(row = 3, column = 1)
+		self.bouton1j = Button(self, text = '1 Joueur', command = self.interface1j)
+		self.bouton1j.grid(row= 2, columnspan = 3)
+		self.bouton2j = Button(self, text = '2 Joueurs', command = self.interface2j)
+		self.bouton2j.grid(row = 3, columnspan = 3)
+
+	def restart_program(self):
+			python = sys.executable
+			os.execl(python, python, * sys.argv)
 
 	def interface1j(self):
 		self.bouton1j.destroy()
 		self.bouton2j.destroy()
 		self.messageD.destroy()
-		self.message1j = Label(self, text = '1 joueur')
+		self.message1j = Label(self, text = '1 Joueur')
 		self.message1j.grid (row = 1, columnspan = 3)
-		self.message_point = Label(self, text = 'Nombre de points = ')
+		self.message_point = Label(self, text = 'Nombre de points =')
 		self.message_point.grid(row = 2, column = 0)
 		self.message_point2 = Label(self, text = self.points)
-		self.message_point2.grid(row = 2, column = 1)
-		self.message_point3 = Label(self,text = 'points')
-		self.message_point3.grid(row = 2, column = 2)
+		self.message_point2.grid(row = 2, column = 1, sticky = W)
 		self.boutonP= Button(self, command = self.pierre)
-		self.boutonP.configure(image = "pierre.gif")
+		self.boutonP.configure(image = pierre)
 		self.boutonP.grid(row = 3, column = 0)
 		self.boutonF = Button(self, text = 'Feuille', command = self.feuille)
-		self.boutonF.configure(image = "papier.gif")
+		self.boutonF.configure(image = feuille)
 		self.boutonF.grid(row = 3, column = 1)
 		self.boutonC = Button(self, text = 'Ciseaux', command =self.ciseaux)
-		self.boutonC.configure(image = "ciseaux.gif")
+		self.boutonC.configure(image = ciseaux)
 		self.boutonC.grid(row = 3, column = 2)
-		self.boutonswitch1 = Button(self, text = 'jouer à 2', command = self.destroyswitch)
+		self.boutonswitch1 = Button(self, text = 'Jouer à 2', command = self.destroyswitch)
 		self.boutonswitch1.grid(row=5, column = 1)
 
 	def destroyswitch(self):
@@ -67,20 +73,22 @@ class Root (Tk):
 		self.message1j.destroy()
 		self.message_point.destroy()
 		self.message_point2.destroy()
-		self.message_point3.destroy()
 		self.points=0
 		self.interface2j()
 
 	def pierre(self):
 		self.choix_joueur = 'pierre'
+		self.imgjoueur = pierre
 		self.choix()
 
 	def feuille(self):
 		self.choix_joueur = 'feuille'
+		self.imgjoueur = feuille
 		self.choix()
 
 	def ciseaux(self):
 		self.choix_joueur = 'ciseaux'
+		self.imgjoueur = ciseaux
 		self.choix()
 
 	def choix(self):
@@ -91,10 +99,13 @@ class Root (Tk):
 		self.ordi = randint(1, 3)
 		if self.ordi == 1:
 			self.ordi = 'pierre'
+			self.imgordi = pierre
 		elif self.ordi == 2:
 			self.ordi = 'feuille'
+			self.imgordi = feuille
 		elif self.ordi == 3:
 			self.ordi = 'ciseaux'
+			self.imgordi = ciseaux
 		self.manche()
 
 	def manche (self):
@@ -125,11 +136,11 @@ class Root (Tk):
 		self.afficher()
 
 	def afficher(self):
-		self.message_joueur = Label(self, text = self.choix_joueur)
+		self.message_joueur = Label(self, image = self.imgjoueur)
 		self.message_joueur.grid(row = 3, column = 0)
-		self.messagevs = Label(self, image = "versus.gif")
+		self.messagevs = Label(self, image = versus)
 		self.messagevs.grid(row = 3, column = 1)
-		self.message_ordi = Label(self, text = self.ordi)
+		self.message_ordi = Label(self, image = self.imgordi)
 		self.message_ordi.grid(row = 3, column = 2)
 		self.message_fin = Label(self,text = '')
 		if self.resultat ==0:
@@ -139,7 +150,7 @@ class Root (Tk):
 			self.message_fin= Label(self, text = 'Tu as gagné')
 			self.message_fin.grid(row = 4, columnspan = 3)
 		elif self.resultat ==1:
-			self.message_fin = Label(self, text = "l'ia a gagné")
+			self.message_fin = Label(self, text = "L'IA a gagné")
 			self.message_fin.grid(row = 4, columnspan = 3)
 		self.bouton_replay = Button(self, text = 'Continuer', command = self.destroy1)
 		self.bouton_replay.grid(row = 5, column = 1)
@@ -151,7 +162,6 @@ class Root (Tk):
 		self.bouton_replay.destroy()
 		self.message_point.destroy()
 		self.message_point2.destroy()
-		self.message_point3.destroy()
 		self.message_fin.destroy()
 		self.message1j.destroy()
 		self.interface1j()
@@ -162,25 +172,25 @@ class Root (Tk):
 		self.bouton1j.destroy()
 		self.bouton2j.destroy()
 		self.messageD.destroy()
-		self.message2j = Label(self, text = '2 joueurs')
+		self.message2j = Label(self, text = '2 Joueurs')
 		self.message2j.grid(row = 1, columnspan = 3)
 		self.message_point = Label(self, text = 'Joueur 1 = ')
 		self.message_point.grid(row = 2, column = 0)
 		self.message_point2 = Label(self, text = self.points2j1)
-		self.message_point2.grid(row = 2, column = 1)
+		self.message_point2.grid(row = 3, column = 0)
 		self.message_pointj2 = Label(self, text = 'Joueur 2 = ')
-		self.message_pointj2.grid(row = 3, column = 0)
+		self.message_pointj2.grid(row = 2, column = 2)
 		self.message_point2j2 = Label(self, text = self.points2j2)
-		self.message_point2j2.grid(row = 3, column = 1)
+		self.message_point2j2.grid(row = 3, column = 2)
 		self.message_joueur1 = Label(self, text = 'Joueur 1')
 		self.message_joueur1.grid(row = 5, columnspan = 3)
-		self.boutonP= Button(self, text = 'Pierre', command = self.pierre1)
+		self.boutonP= Button(self, image = pierre, command = self.pierre1)
 		self.boutonP.grid(row = 6, column = 0)
-		self.boutonF = Button(self, text = 'Feuille', command = self.feuille1)
+		self.boutonF = Button(self, image = feuille, command = self.feuille1)
 		self.boutonF.grid(row = 6,column = 1)
-		self.boutonC = Button(self, text = 'Ciseaux', command =self.ciseaux1)
+		self.boutonC = Button(self, image = ciseaux, command =self.ciseaux1)
 		self.boutonC.grid(row = 6, column = 2)
-		self.boutonswitch2 = Button(self, text = 'jouer seul', command = self.destroyswitch2j)
+		self.boutonswitch2 = Button(self, text = 'Jouer seul', command = self.destroyswitch2j)
 		self.boutonswitch2.grid(row = 8, column = 1)
 
 	def destroyswitch2j(self):
@@ -200,14 +210,17 @@ class Root (Tk):
 
 	def pierre1(self):
 		self.choixj1 = 'pierre'
+		self.choosej1 = pierre
 		self.joueur2()
 
 	def feuille1(self):
 		self.choixj1 = 'feuille'
+		self.choosej1 = feuille
 		self.joueur2()
 
 	def ciseaux1(self):
 		self.choixj1 = 'ciseaux'
+		self.choosej1 = ciseaux
 		self.joueur2()
 
 	def joueur2 (self):
@@ -218,23 +231,26 @@ class Root (Tk):
 		self.boutonswitch2.destroy()
 		self.message_joueur2 = Label(self, text = 'Joueur 2')
 		self.message_joueur2.grid(row = 5, columnspan = 3)
-		self.boutonP2= Button(self, text = 'Pierre', command = self.pierre2)
+		self.boutonP2= Button(self, image = pierre, command = self.pierre2)
 		self.boutonP2.grid(row = 6, column = 0)
-		self.boutonF2 = Button(self, text = 'Feuille', command = self.feuille2)
+		self.boutonF2 = Button(self, image = feuille, command = self.feuille2)
 		self.boutonF2.grid(row = 6, column = 1)
-		self.boutonC2 = Button(self, text = 'Ciseaux', command =self.ciseaux2)
+		self.boutonC2 = Button(self, image = ciseaux, command =self.ciseaux2)
 		self.boutonC2.grid(row = 6, column = 2)
 
 	def pierre2(self):
 		self.choixj2 = 'pierre'
+		self.choosej2 = pierre
 		self.manche2j()
 
 	def feuille2(self):
 		self.choixj2 = 'feuille'
+		self.choosej2 = feuille
 		self.manche2j()
 
 	def ciseaux2(self):
 		self.choixj2 = 'ciseaux'
+		self.choosej2 = ciseaux
 		self.manche2j()
 
 	def manche2j (self):
@@ -269,11 +285,11 @@ class Root (Tk):
 		self.afficher2j()
 
 	def afficher2j(self):
-		self.message_j1 = Label(self, text = self.choixj1)
+		self.message_j1 = Label(self, image = self.choosej1)
 		self.message_j1.grid(row = 6, column = 0)
-		self.messagevs = Label(self, text = 'vs')
+		self.messagevs = Label(self, image = versus)
 		self.messagevs.grid(row = 6, column = 1)
-		self.message_j2= Label(self, text = self.choixj2)
+		self.message_j2= Label(self, image = self.choosej2)
 		self.message_j2.grid(row = 6, column = 2)
 		if self.resultat ==0:
 			self.message_fin = Label(self, text = 'Egalité')
@@ -282,7 +298,7 @@ class Root (Tk):
 			self.message_fin= Label(self, text = 'Le joueur 1 a gagné')
 			self.message_fin.grid(row = 7, columnspan = 3)
 		elif self.resultat ==1:
-			self.message_fin = Label(self, text = "le joueur 2 a gagné")
+			self.message_fin = Label(self, text = "Le joueur 2 a gagné")
 			self.message_fin.grid(row = 7, columnspan = 3)
 		self.bouton_replay = Button(self, text = 'Continuer', command = self.destroy2j)
 		self.bouton_replay.grid(row=8, column = 1)
@@ -341,6 +357,11 @@ SOFTWARE.""", font = "Ar")
 			self.fen.destroy()
 
 
-
 fenetre = Root()
+versus = PhotoImage(file = "versus.gif")
+pierre = PhotoImage(file = "pierre.gif")
+feuille = PhotoImage(file = 'papier.gif')
+ciseaux = PhotoImage(file = "ciseaux.gif")
+
 fenetre.mainloop()
+
